9e1c0f06a65e2c24a548782e7b0995b6f404ab10
iammostwanted <2909Serik@gmail.com>
2026-01-06 02:57:02 +0500

    Build fix

--- scripts/app.mjs
+++ scripts/app.mjs
@@ -3,7 +3,7 @@ import {
     PublishCommand
 } from "@aws-sdk/client-sns";
 
-const TOPIC_ARN_NAME = "arn:aws:sns:us-east-1:757377850179:AT-UploadsNotificationTopic";
+const TOPIC_ARN_NAME = "arn:aws:sns:us-east-1:043254348746:DemoTopic";
 const REGION = "us-east-1";
 
 const sns = new SNSClient({
